354f31d4b2be8880dca30e8b12a6439d1f11b533
kasiwa-mesi <italynataly@outlook.jp>
2022-10-17 15:27:09 +0900

    :hammer: NWPathMonitorを使って、オフラインの場合の処理を記述

---
+++ QiitaAPISample/ViewController/Offline/OfflineViewController.swift
@@ -0,0 +1,14 @@
+//
+//  OfflineViewController.swift
+//  QiitaAPISample
+//
+//  Created by kasiwa on 2022/10/17.
+//
+
+import UIKit
+
+final class OfflineViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
